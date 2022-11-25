2f69f772026ac73db0c0c63d5b5318dcf479b36d
sahneisttoll <sahneisttoll@gmail.com>
2022-11-25 18:00:04 +0100

    lil adjust

--- extension/lua/joypad-assist/CombineScript/settings.lua
+++ extension/lua/joypad-assist/CombineScript/settings.lua
@@ -1,19 +1,19 @@
 Race = {
 	stopAutoClutch		= 1,	-- 0-1	| Automatic clutch when stopping.
-	handbrakeClutchLink	= 0, 	-- 0-1	| Engages the clutch when the handbrake is pulled.
+	handbrakeClutchLink	= 1,	-- 0-1	| Engages the clutch when the handbrake is pulled.
 
 -- DualSense5 Settings (Dualsense controller will be auto detected)
-	Dualsense5_strength	= 100, 	-- 0-100| Strength of DualSense Adaptive Triggers
-	Dualsense5_TC		= 1,	-- 0-1	| Right Adaptive Trigger
-	Dualsense5_ABS		= 1		--0-1	| Left Adaptive Trigger
+	Dualsense5_strength	= 0,	-- 0-100| Strength of DualSense Adaptive Triggers
+	Dualsense5_TC		= 0,	-- 0-1	| Right Adaptive Trigger
+	Dualsense5_ABS		= 0,	-- 0-1	| Left Adaptive Trigger
 }
 
 Drift = {
-	stopAutoClutch		= 1,	-- 0-1	| Automatic clutch when stopping.
-	handbrakeClutchLink	= 0, 	-- 0-1	| Engages the clutch when the handbrake is pulled.
+	stopAutoClutch		= 0,	-- 0-1	| Automatic clutch when stopping.
+	handbrakeClutchLink	= 1,	-- 0-1	| Engages the clutch when the handbrake is pulled.
 
 -- DualSense5 Settings (Dualsense controller will be auto detected)
 	Dualsense5_strength	= 0, 	-- 0-100| Strength of DualSense Adaptive Triggers
 	Dualsense5_TC		= 0,	-- 0-1	| Right Adaptive Trigger
-	Dualsense5_ABS		= 0		--0-1	| Left Adaptive Trigger
+	Dualsense5_ABS		= 0,	-- 0-1	| Left Adaptive Trigger
 }
